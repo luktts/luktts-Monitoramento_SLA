5f7e36fcda48d20ee2ce8ffa25bcc9c0bdd8e11b
luktts <luksmattos1996@gmail.com>
2026-04-01 21:44:18 -0300

    Create grupos.py

---
+++ config/grupos.py
@@ -0,0 +1,37 @@
+# este código armazena os grupos de suporte da empresa
+
+groups = {
+    "Servidores": [
+        "Servidores-Support",
+        "Servidores-Nuvem",
+        "Servidores-Especialista"
+    ],
+
+    "Support": [
+        "Support-Usuario",
+        "Support-Tecnico",
+        "Support-Especialista",
+        "Support-Laptops",
+        "Support-Workstation",
+        "Support-Mobile"
+    ],
+
+    "ServiceDesk": [
+        "Service-Desk"
+    ],
+
+    "Telecom": [
+        "Telecom-Support",
+        "Telecom-Cabling",
+        "Telecom-Especialista"
+    ],
+
+    "Microsoft-Support": [
+        "MS-PowerPoint",
+        "MS-Excel",
+        "MS-PowerBI",
+        "MS-Teams",
+        "MS-Word"
+    ]
+
+}
