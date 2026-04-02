274930ad235bb46ee8b41d63e4eabb7b2c79dbe2
luktts <luksmattos1996@gmail.com>
2026-04-01 21:43:51 -0300

    Create contact.py

---
+++ config/contact.py
@@ -0,0 +1,58 @@
+"""
+Este código tem como objetivo armazenar informações
+de contato do manager e techleader para alertar sobre
+SLA
+"""
+
+contact_teams = {
+    "Servidores": {
+        "manager_name": "Manager Servidores",
+        "manager_email": "Manager.Servidores@email.com",
+
+        "tech_leader_name": "Leader Servidores",
+        "tech_leader_email": "Leader.Servidores@email.com",
+
+        "INC_link": "www.linksuperreal.com.br/naoexiste-Servidores"
+    },
+
+    "Support": {
+        "manager_name": "Manager Suppport",
+        "manager_email": "Manager.Support@email.com",
+
+        "tech_leader_name": "Leader Support",
+        "tech_leader_email": "Leader.Support@email.com",
+
+        "INC_link": "www.linksuperreal.com.br/naoexiste-Support"
+    },
+
+    "ServiceDesk": {
+        "manager_name": "ServiceDesk Manager",
+        "manager_email": "ServiceDesk.Manager@email.com",
+
+        "tech_leader_name": "Leader ServiceDesk",
+        "tech_leader_email": "Leader.ServiceDesk@email.com",
+
+        "INC_link": "www.linksuperreal.com.br/naoexiste-ServiceDesk"
+
+    },
+
+    "Telecom": {
+        "manager_name": "Manager Telecom",
+        "manager_email": "Manager.Telecom@email.com",
+
+        "tech_leader_name": "Leader Telecom",
+        "tech_leader_email": "Leader.Telecom@email.com",
+
+        "INC_link": "www.linksuperreal.com.br/naoexiste-Telecom"
+    },
+
+    "Microsoft-Support": {
+        "manager_name": "Phil Spencer",
+        "manager_email": "Phil.Spencer@email.com",
+
+        "tech_leader_name": "Sarah Bond",
+        "tech_leader_email": "Sarah.Bond@email.com",
+
+        "INC_link": "www.linksuperreal.com.br/naoexiste-MS-Support"
+    },
+}
